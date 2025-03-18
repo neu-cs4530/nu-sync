ec8408d2c7755cfd251ffecc6ed2e3d9073d1898
Shreypatel2224 <87133722+Shreypatel2224@users.noreply.github.com>
2025-03-18 17:39:36 -0400

    question.d.ts updated for polls

--- shared/types/question.d.ts
+++ shared/types/question.d.ts
@@ -5,6 +5,26 @@ import { DatabaseTag, Tag } from './tag';
 import { Comment, DatabaseComment } from './comment';
 
 /**
+ * Represents a poll option.
+ * - `optionText`: The text of the option.
+ * - `votes`: An array of usernames who voted for this option.
+ */
+export interface PollOption {
+  optionText: string;
+  votes: string[];
+}
+
+/**
+ * Represents a poll.
+ * - `question`: The poll question.
+ * - `options`: An array of poll options.
+ */
+export interface Poll {
+  question: string;
+  options: PollOption[];
+}
+
+/**
  * Type representing the possible ordering options for questions.
  * - `newest`: Sort by the most recently asked questions.
  * - `unanswered`: Sort by questions with no answers.
@@ -25,6 +45,7 @@ export type OrderType = 'newest' | 'unanswered' | 'active' | 'mostViewed';
  * - `upVotes`: An array of usernames who have upvoted the question.
  * - `downVotes`: An array of usernames who have downvoted the question.
  * - `comments`: An array of comments related to the question.
+ * - `poll`: An optional poll associated with the question.
  */
 export interface Question {
   title: string;
@@ -37,6 +58,7 @@ export interface Question {
   upVotes: string[];
   downVotes: string[];
   comments: Comment[];
+  poll?: Poll;
 }
 
 /**
@@ -45,12 +67,14 @@ export interface Question {
  * - `tags`: An array of ObjectIds referencing tags associated with the question.
  * - `answers`: An array of ObjectIds referencing answers associated with the question.
  * - `comments`: An array of ObjectIds referencing comments associated with the question.
+ * - 'poll': An optional poll associated with the question.
  */
 export interface DatabaseQuestion extends Omit<Question, 'tags' | 'answers' | 'comments'> {
   _id: ObjectId;
   tags: ObjectId[];
   answers: ObjectId[];
   comments: ObjectId[];
+  poll?: Poll;
 }
 
 /**
@@ -58,12 +82,14 @@ export interface DatabaseQuestion extends Omit<Question, 'tags' | 'answers' | 'c
  * - `tags`: An array of populated `DatabaseTag` objects.
  * - `answers`: An array of populated `PopulatedDatabaseAnswer` objects.
  * - `comments`: An array of populated `DatabaseComment` objects.
+ * - 'poll': An optional poll associated with the question.
  */
 export interface PopulatedDatabaseQuestion
   extends Omit<DatabaseQuestion, 'tags' | 'answers' | 'comments'> {
   tags: DatabaseTag[];
   answers: PopulatedDatabaseAnswer[];
   comments: DatabaseComment[];
+  poll?: Poll;
 }
 
 /**
