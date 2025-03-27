4e494015c8ac36e8c26d2aab44b4dfd75defa69e
Jason Ie <ie.jasonray10@gmail.com>
2025-03-26 23:50:13 -0400

    Removed testing logs in hook

--- client/src/hooks/useFriendRequests.ts
+++ client/src/hooks/useFriendRequests.ts
@@ -36,16 +36,16 @@ const useFriendRequests = () => {
 
         // Fetch all requests for the current user
         if (user.username) {
-          console.log('Fetching requests for user:', user.username);
+          // console.log('Fetching requests for user:', user.username);
 
           const requests = await getFriendRequests(user.username);
-          console.log('Requests fetched:', requests);
+          // console.log('Requests fetched:', requests);
 
           setAllRequests(requests);
 
           // Fetch pending requests for the current user
           const pending = await getPendingRequests(user.username);
-          console.log('Pending requests:', pending);
+          // console.log('Pending requests:', pending);
           setPendingRequests(pending);
         }
       } catch (err) {
